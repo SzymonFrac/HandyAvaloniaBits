d3cfde2295a50ae5c1165a546181140082aef524
SzymonFrac <szymon.frac195@gmail.com>
2026-07-16 14:36:40 +0100

    Implemented morphing from a line segment to an arc segment.

--- HandyAvaloniaBits.Animations/Morph/Segment/Implementations/MorphLineToArc.cs
+++ HandyAvaloniaBits.Animations/Morph/Segment/Implementations/MorphLineToArc.cs
@@ -7,6 +7,8 @@ using HandyAvaloniaBits.Animations.Morph.AvaloniaRotation.Lerp;
 using HandyAvaloniaBits.Animations.Morph.AvaloniaSize.Ext;
 using HandyAvaloniaBits.Animations.Morph.AvaloniaSize.Lerp;
 using HandyAvaloniaBits.Animations.Morph.Segment.Abstract;
+using ReactiveUI;
+using System.Diagnostics;
 
 namespace HandyAvaloniaBits.Animations.Morph.Segment.Implementations;
 
@@ -15,9 +17,19 @@ internal sealed record MorphLineToArc : MorphToArc
     private MorphLineToArc(MorphPointLerp lerp, MorphSizeLerp sizeLerp, MorphRotationLerp rotation, ArcSegment toArc)
         : base(lerp, sizeLerp, rotation, toArc) { }
 
-    public static MorphLineToArc Create(in LineSegment from, in ArcSegment to, ref (Point from, Point to) start) =>
-        new(from.Point.LerpTo((start = (from.Point, to.Point)).to),
-            default(Size).LerpTo(to.Size),
-            0.0.LerpTo(to.RotationAngle),
-            to);
+    public static MorphLineToArc Create(in LineSegment from, in ArcSegment to, ref (Point from, Point to) start)
+    {
+        var pointLerp = from.Point.LerpTo(to.Point);
+
+        var distance = from.Point - start.from;
+        var diameter = Math.Sqrt(distance.X * distance.X + distance.Y * distance.Y);
+        var ellipseStartSize = new Size(diameter / 2, 0);
+        var sizeLerp = ellipseStartSize.LerpTo(to.Size);
+
+        var startRotation = Math.Atan2(from.Point.Y - start.from.Y, from.Point.X - start.from.X) * 180 / Math.PI;
+        var rotationLerp = startRotation.LerpTo(to.RotationAngle);
+
+        start = (from.Point, to.Point);
+        return new(pointLerp, sizeLerp, rotationLerp, to);
+    }
 }
